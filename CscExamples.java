1b8740ae37850ea4526c4f6c667bff2213e00a08
Supasthiami <nishanilingam27@gmail.com>
2020-09-18 04:42:26 +0530

    Second commit

--- CscExamples.java
+++ CscExamples.java
@@ -3,4 +3,37 @@ class CscExamples{
 	String category;
 	String text;
 	
+	public void setQuestionNo(int qNo){
+		
+	}
+	public void setCategory(int caNo){
+		
+	}
+	public void setText(int textNo){
+		
+	}
+	public int getQuestionNo()
+	{
+		
+	}
+	public int getCategory()
+	{
+		
+	}
+	public int getText()
+	{
+		
+	}
+	public void addQuestion(CscExample qu)
+	{
+		
+	}
+	public void removeQuestion(int quId)
+	{
+		
+	}
+	public void editQuestion(int quId)
+	{
+		
+	}
 }
